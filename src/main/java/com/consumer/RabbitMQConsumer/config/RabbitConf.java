abcf451cfd26709346763d374d8c508ba21863a8
Erik <78443570+thekhachik@users.noreply.github.com>
2023-03-31 18:45:09 +0300

    Initial commit

---
+++ src/main/java/com/consumer/RabbitMQConsumer/config/RabbitConf.java
@@ -0,0 +1,50 @@
+package com.consumer.RabbitMQConsumer.config;
+
+import com.rabbitmq.client.ConnectionFactory;
+import org.springframework.amqp.core.AmqpAdmin;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.amqp.core.*;
+
+@Configuration
+public class RabbitConf {
+
+    @Bean
+    public CachingConnectionFactory connectionFactory() {
+        CachingConnectionFactory cachingConnectionFactory = new CachingConnectionFactory("localhost");
+        cachingConnectionFactory.setUsername("rmuser");
+        cachingConnectionFactory.setPassword("rmpassword");
+        cachingConnectionFactory.setVirtualHost("cpp");
+        return cachingConnectionFactory;
+    }
+
+    @Bean
+    public AmqpAdmin amqpAdmin() {
+        return new RabbitAdmin(connectionFactory());
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate() {
+        return new RabbitTemplate(connectionFactory());
+    }
+
+    @Bean
+    public Queue myQueue() {
+        return new Queue("queue1");
+    }
+
+    @Bean
+    DirectExchange exchange() {
+        return new DirectExchange("testExchange", true, false);
+    }
+
+    @Bean
+    Binding binding(Queue queue, DirectExchange exchange) {
+        return BindingBuilder.bind(queue).to(exchange).with("testRoutingKey");
+    }
+}
---
+++ src/test/java/com/consumer/RabbitMQConsumer/RabbitMqConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.consumer.RabbitMQConsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitMqConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
